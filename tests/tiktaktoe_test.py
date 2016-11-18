98a00745e73a95ae21604a4f02bfcb07f6999b50
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 14:58:54 -0800

    write second unit test

--- tests/tiktaktoe_test.py
+++ tests/tiktaktoe_test.py
@@ -3,8 +3,14 @@ from source.game_board import *
 
 
 
-def test_game_board_returns_a_list():
+def test_game_board_returns_a_2Dlist():
     board = Game_Board()
     board_right_now = getattr(board, 'current_board')
     test = all(isinstance(row, list) for row in board_right_now)
     assert test ==  True
+
+def test_game_board_initial_state():
+    board = Game_Board()
+    test = getattr(board, 'current_board')
+    expected = [[0,0,0],[0,0,0],[0,0,0]]
+    assert test == expected
