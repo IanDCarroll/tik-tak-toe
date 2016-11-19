fd3ebe06b436eed14904e909ec08e20e8241e43a
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 17:59:31 -0800

    test if player can make a move

--- tests/player_chair_test.py
+++ tests/player_chair_test.py
@@ -1,5 +1,8 @@
 import nose.tools
 from source.player_chair import *
 
-def test_that_tests_work():
-    assert True == True
+def test_that_player_can_make_a_move():
+    philip = Player()
+    mock_board = [[1,10,1],[10,0,0],[1,0,10]]
+    test = philip.move(mock_board)
+    assert test == [[1,10,1],[10,1,0],[1,0,10]]
