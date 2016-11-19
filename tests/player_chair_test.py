abfc03737e503fed8542fc0467242557c82cf96e
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 20:35:41 -0800

    test that players can only make legal moves

--- tests/player_chair_test.py
+++ tests/player_chair_test.py
@@ -7,6 +7,12 @@ def test_that_player_can_make_a_move():
     test = philip.move(mock_board)
     assert test == [1,10,1,10,"spam",0,1,0,10]
 
+def test_that_player_can_only_make_legal_moves():
+    philip = Player()
+    mock_board = [1,10,1,10,0,0,1,0,10]
+    test = philip.get_legal_moves(mock_board)
+    assert test == [4,5,7]
+
 def test_that_computer_player_can_make_a_move():
     spamBot9000 = Computer()
     mock_board = [1,10,1,0,10,0,1,0,10]
