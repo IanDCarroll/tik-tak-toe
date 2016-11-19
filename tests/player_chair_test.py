373a1deef0dcaff26476b888d54197db99e2e572
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 21:19:11 -0800

    test if human has only legal choices

--- tests/player_chair_test.py
+++ tests/player_chair_test.py
@@ -18,3 +18,10 @@ def test_that_computer_player_can_make_a_move():
     mock_board = [1,10,1,0,10,0,1,0,10]
     test = spamBot9000.move(mock_board)
     assert test == [1,10,1,1,10,0,1,0,10]
+
+def test_that_human_can_only_make_legal_moves():
+    bernice = Human()
+    test_true = bernice.check_if_legit(7)
+    test_false = bernice.check_if_legit(4)
+    assert test_true == True
+    assert test_false == False
