6c842500c6ac7afa24b4ee57867479dc2d0840cf
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 20:13:10 -0800

    change player tests to reflect game_table change

--- tests/player_chair_test.py
+++ tests/player_chair_test.py
@@ -3,12 +3,12 @@ from source.player_chair import *
 
 def test_that_player_can_make_a_move():
     philip = Player()
-    mock_board = [[1,10,1],[10,0,0],[1,0,10]]
+    mock_board = [1,10,1,10,0,0,1,0,10]
     test = philip.move(mock_board)
-    assert test == [[1,10,1],[10,"spam",0],[1,0,10]]
+    assert test == [1,10,1,10,"spam",0,1,0,10]
 
 def test_that_computer_player_can_make_a_move():
     spamBot9000 = Computer()
-    mock_board = [[1,10,1],[0,10,0],[1,0,10]]
+    mock_board = [1,10,1,0,10,0,1,0,10]
     test = spamBot9000.move(mock_board)
-    assert test == [[1,10,1],[1,10,0],[1,0,10]]
+    assert test == [1,10,1,1,10,0,1,0,10]
