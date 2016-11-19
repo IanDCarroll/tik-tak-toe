df9fd069d93dca337e080ba388abba05a7888a26
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 21:03:37 -0800

    refactor tests with mock TableTop board

--- tests/player_chair_test.py
+++ tests/player_chair_test.py
@@ -1,20 +1,24 @@
 import nose.tools
 from source.player_chair import *
 
+class Mock(object):
+    def __init__(self):
+        self.board = [1,10,1,10,0,0,1,0,10]
+
 def test_that_player_can_make_a_move():
+    mock = Mock()
     philip = Player()
-    mock_board = [1,10,1,10,0,0,1,0,10]
-    test = philip.move(mock_board)
+    test = philip.move(mock.board)
     assert test == [1,10,1,10,"spam",0,1,0,10]
 
 def test_that_player_can_only_make_legal_moves():
+    mock = Mock()
     philip = Player()
-    mock_board = [1,10,1,10,0,0,1,0,10]
-    test = philip.get_legal_moves(mock_board)
+    test = philip.get_legal_moves(mock.board)
     assert test == [4,5,7]
 
 def test_that_computer_player_can_make_a_move():
+    mock = Mock()
     spamBot9000 = Computer()
-    mock_board = [1,10,1,0,10,0,1,0,10]
-    test = spamBot9000.move(mock_board)
+    test = spamBot9000.move(mock.board)
     assert test == [1,10,1,1,10,0,1,0,10]
