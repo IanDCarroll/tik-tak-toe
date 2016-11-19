7a4db5f22357d39308ae8cb6a5644316799978e9
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 17:25:19 -0800

    refactor test function names

--- tests/game_table_test.py
+++ tests/game_table_test.py
@@ -3,17 +3,17 @@ from source.game_table import *
 
 
 
-def test_game_board_returns_a_2Dlist():
+def test_board_returns_a_2Dlist():
     table_top = TableTop()
     test = all(isinstance(row, list) for row in table_top.board)
     assert test ==  True
 
-def test_game_board_initial_state():
+def test_the_boards_initial_state():
     table_top = TableTop()
     expected = [[0,0,0],[0,0,0],[0,0,0]]
     assert table_top.board == expected
 
-def test_game_board_can_be_changed():
+def test_that_board_can_be_changed():
     table_top = TableTop()
     expected = [[0,0,1],[0,0,0],[0,0,0]]
     table_top.board = expected
