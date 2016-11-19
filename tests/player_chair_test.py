ec5218802f7c84001a2fbbe969eb75a1cc30ffde
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 18:40:33 -0800

    rewite test for player class

--- tests/player_chair_test.py
+++ tests/player_chair_test.py
@@ -5,7 +5,7 @@ def test_that_player_can_make_a_move():
     philip = Player()
     mock_board = [[1,10,1],[10,0,0],[1,0,10]]
     test = philip.move(mock_board)
-    assert test == [[1,10,1],[10,1,0],[1,0,10]]
+    assert test == [[1,10,1],[10,"spam",0],[1,0,10]]
 
 def test_that_computer_player_can_make_a_move():
     spamBot9000 = Computer()
