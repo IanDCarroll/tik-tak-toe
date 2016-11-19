eee667c97e641bdc6762f551c94a08f0811abd70
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-19 09:26:05 -0800

    add next_move message to display

--- source/display.py
+++ source/display.py
@@ -5,6 +5,7 @@ class Display():
         self.draw = "Draw"
         self.computer = "Computer Wins"
         self.human = "You Win"
+        self.next_move = "What is your next move?"
 
     def show(self, text):
         print text
