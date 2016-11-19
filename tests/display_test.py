425e2d2e13b95189414b655f093e82fc007debf8
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-19 08:47:49 -0800

    add empty test cases for display

---
+++ tests/display_test.py
@@ -0,0 +1,26 @@
+import nose.tools
+from source.display import *
+
+def test_display_start_of_game():
+    pass
+
+def test_display_game_over_draw():
+    pass
+
+def test_display_game_over_computer_wins():
+    pass
+
+def test_display_game_over_human_wins():
+    pass
+
+def test_display_current_board():
+    pass
+
+def test_display_computers_last_move():
+    pass
+
+def test_display_ask_for_human_input():
+    pass
+
+def test_display_human_made_illegal_move():
+    pass
