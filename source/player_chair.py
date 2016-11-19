c71e0b74589b2f87ada60e72a0c7413615cc3faf
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 20:41:53 -0800

    write get_legal_moves method for players

--- source/player_chair.py
+++ source/player_chair.py
@@ -3,6 +3,13 @@ class Player(object):
         board[4] = "spam"
         return board
 
+    def get_legal_moves(self, board):
+        legal_moves = []
+        for i in range(0, len(board)):
+            if board[i] == 0:
+                legal_moves.append(i)
+        return legal_moves
+
 class Human(Player):
     def move(self, board):
         square = raw_input("Which square do you choose? ")
