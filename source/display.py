53a9759841e56e11bcbfdb381125e1cd1735d8a2
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-19 09:10:34 -0800

    add computer wins to display messages

--- source/display.py
+++ source/display.py
@@ -3,6 +3,7 @@ class Display():
     def __init__(self):
         self.start = "Welcome"
         self.draw = "Draw"
+        self.computer = "Computer Wins"
 
     def show(self, text):
         print text
