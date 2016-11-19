c4bac4921fbce167198f4ce925e61d7abaad8867
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 18:13:23 -0800

    write untested human player

--- source/player_chair.py
+++ source/player_chair.py
@@ -2,3 +2,10 @@ class Player(object):
     def move(self, board):
         board[1][1] = 1
         return board
+
+class Human(Player):
+    def move(self, board):
+        row = raw_input("Which row do you choose? ")
+        col = raw_input("Which column do you choose? ")
+        board[row][col] = 1
+        return board
