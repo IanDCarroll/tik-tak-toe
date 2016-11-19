b7671da38b0aa401406d1ec72a9f9627ebfdf032
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-19 12:27:33 -0800

    abstract board stringification from render_board

--- source/display.py
+++ source/display.py
@@ -12,6 +12,15 @@ class Display():
         print text
 
     def render_board(self, board):
+        board = self.stringify_board(board)
+        line1 = ' ' + board[0] + ' | ' + board[1] + ' | ' + board[2] + ' \n'
+        line2 = ' ' + board[3] + ' | ' + board[4] + ' | ' + board[5] + ' \n'
+        line3 = ' ' + board[6] + ' | ' + board[7] + ' | ' + board[8] + ' \n'
+        wall = "---+---+---\n"
+        rendered_board = '\n' + line1 + wall + line2 + wall + line3
+        return rendered_board
+
+    def stringify_board(self, board):
         for i in range(0, len(board)):
             if board[i] == 0:
                 board[i] = " "
@@ -19,9 +28,5 @@ class Display():
                 board[i] = "X"
             elif board[i] == 10:
                 board[i] = "O"
-        line1 = ' ' + board[0] + ' | ' + board[1] + ' | ' + board[2] + ' \n'
-        line2 = ' ' + board[3] + ' | ' + board[4] + ' | ' + board[5] + ' \n'
-        line3 = ' ' + board[6] + ' | ' + board[7] + ' | ' + board[8] + ' \n'
-        wall = "---+---+---\n"
-        rendered_board = '\n' + line1 + wall + line2 + wall + line3
-        return rendered_board
+        return board
+
