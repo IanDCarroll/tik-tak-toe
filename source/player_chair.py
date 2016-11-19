d2664b9bf1e989d5d80ffce53964d280a3ee6e67
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-18 20:16:50 -0800

    change Player and Human to single input

--- source/player_chair.py
+++ source/player_chair.py
@@ -1,13 +1,12 @@
 class Player(object):
     def move(self, board):
-        board[1][1] = "spam"
+        board[4] = "spam"
         return board
 
 class Human(Player):
     def move(self, board):
-        row = raw_input("Which row do you choose? ")
-        col = raw_input("Which column do you choose? ")
-        board[row][col] = 10
+        square = raw_input("Which square do you choose? ")
+        board[square] = 10
         return board
 
 class Computer(Player):
