71097613f6a9533d02beb5ca5c4f819edd95eac9
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-19 09:31:49 -0800

    add illegal move message to display

--- source/display.py
+++ source/display.py
@@ -6,6 +6,7 @@ class Display():
         self.computer = "Computer Wins"
         self.human = "You Win"
         self.next_move = "What is your next move?"
+        self.bad_move = "That is not a legal move."
 
     def show(self, text):
         print text
