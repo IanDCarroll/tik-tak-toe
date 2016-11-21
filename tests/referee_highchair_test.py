a12ebdc2ffca030179b71767dc45ad59c1a8edc0
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-20 19:57:41 -0800

    test if check for draw can do so

--- tests/referee_highchair_test.py
+++ tests/referee_highchair_test.py
@@ -4,3 +4,11 @@ from source.referee_highchair import *
 def test_referee_is_an_object():
     referee = Referee()
     assert isinstance(referee, object) == True
+
+def test_check_for_draw_can_do_so():
+    referee = Referee()
+    false_board = [1,10,1,0,10,0,1,0,10]
+    true_board = [1,1,10,10,10,1,1,10,1]
+    assert referee.check_for_draw(false_board) == False 
+    assert referee.check_for_draw(true_board) == True
+
