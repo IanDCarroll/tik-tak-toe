a0ec034ee1b5265fa38399c8c3728cd7fd900935
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-20 21:12:42 -0800

    break apart massive test

--- tests/referee_highchair_test.py
+++ tests/referee_highchair_test.py
@@ -5,27 +5,37 @@ def test_referee_is_an_object():
     referee = Referee('fake_board','fake_P1', 'fake_p2')
     assert isinstance(referee, object) == True
 
-def test_check_for_draw_can_do_so():
-    false_board = [1,10,1,0,10,0,1,0,10]
+def test_check_for_draw_returns_true():
     true_board = [1,1,10,10,10,1,1,10,1]
-    false_ref = Referee(false_board, 'P1', 'P2')
     true_ref = Referee(true_board, 'P1', 'P2')
-    assert false_ref.check_for_draw() == False 
     assert true_ref.check_for_draw() == True
 
-def test_check_for_winner_can_do_so():
+def test_check_for_draw_returns_false():
     false_board = [1,10,1,0,10,0,1,0,10]
-    edge_board = [1,1,10,10,10,1,1,10,1]
-    center_board = [1,10,1,0,10,0,1,10,0]
-    diagonal_board = [1,10,1,10,1,0,1,0,10]
-    cross_board = [1,0,1,10,10,10,0,0,1]
     false_ref = Referee(false_board, 'P1', 'P2')
-    edge_ref = Referee(edge_board, 'P1', 'P2')
-    center_ref = Referee(center_board, 'P1', 'P2')
-    diagonal_ref = Referee(diagonal_board, 'P1', 'P2')
-    cross_ref = Referee(cross_board, 'P1', 'P2')
+    assert false_ref.check_for_draw() == False 
+
+def test_check_for_winner_returns_false():
+    false_board = [1,10,1,0,10,0,1,0,10]
+    false_ref = Referee(false_board, 'P1', 'P2')
     assert false_ref.check_for_winner() == False 
+
+def test_check_for_winner_does_edges():
+    edge_board = [1,1,10,10,10,1,1,10,1]
+    edge_ref = Referee(edge_board, 'P1', 'P2')
     assert edge_ref.check_for_winner() == True
-    assert center_ref.check_for_winner() == True
+
+def test_check_for_winner_does_columns():
+    column_board = [1,10,1,0,10,0,1,10,0]
+    column_ref = Referee(column_board, 'P1', 'P2')
+    assert column_ref.check_for_winner() == True
+
+def test_check_for_winner_does_rows():
+    row_board = [1,0,1,10,10,10,0,0,1]
+    row_ref = Referee(row_board, 'P1', 'P2')
+    assert row_ref.check_for_winner() == True
+
+def test_check_for_winner_does_diagonals():
+    diagonal_board = [1,10,1,10,1,0,1,0,10]
+    diagonal_ref = Referee(diagonal_board, 'P1', 'P2')
     assert diagonal_ref.check_for_winner() == True
-    assert cross_ref.check_for_winner() == True
