8b03d889109f50a6e99adc7bc32c1560c2ce3a5b
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-21 22:54:36 -0800

    refactor referee and tests passing TableTop as arg

--- source/referee_chair.py
+++ source/referee_chair.py
@@ -5,21 +5,21 @@ import announcer_chair
 
 class Referee(Facilitator):
 
-    def __init__(self, board, player1, player2):
-        self.board = board
+    def __init__(self, tabletop, player1, player2):
+        self.tabletop = tabletop
         self.player1 = player1
         self.player2 = player2
         self.whos_turn = self.player1
 
     def start_game(self):
         announcer.show(announcer.start)
-        announcer.show(announcer.render_board(self.board))
+        announcer.show(announcer.render_board(self.tabletop.board))
         self.facilitate_turns()
 
     def facilitate_turns(self):
         self.whos_turn.move(self.board)
-        announcer.show(announcer.render_board(self.board))
-        the_game_is_over = check_for_game_over(self.board)
+        announcer.show(announcer.render_board(self.tabletop.board))
+        the_game_is_over = check_for_game_over(self.tabletop.board)
         if the_game_is_over != False:
             game_over(the_game_is_over)
         else:
@@ -33,7 +33,7 @@ class Referee(Facilitator):
         facilitate_turns()
         
     def game_over(self, winner):
-        announcer.show(announcer.render_board(self.board))
+        announcer.show(announcer.render_board(self.tabletop.board))
         if winner == "tie":
             announcer.show(display.draw)
         elif winner == "computer":
@@ -50,13 +50,13 @@ class Referee(Facilitator):
             return False
 
     def check_for_tie(self):
-        if 0 in self.board:
+        if 0 in self.tabletop.board:
             return False
         else:
             return True
 
     def check_for_winner(self):
-        board = self.board
+        board = self.tabletop.board
         scan_list = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],
                      [1,4,7],[2,5,8],[0,4,8],[2,4,6]]
         did_they_win = False
--- tests/referee_chair_test.py
+++ tests/referee_chair_test.py
@@ -1,53 +1,63 @@
 import nose.tools
 from source.referee_chair import *
+from source.game_table import * 
 
 def test_referee_is_an_object():
     referee = Referee('fake_board','fake_P1', 'fake_p2')
     assert isinstance(referee, object) == True
 
 def test_check_for_draw_returns_true():
-    true_board = [1,1,10, 10,10,1, 1,10,1]
+    true_board = TableTop()
+    true_board.board = [1,1,10, 10,10,1, 1,10,1]
     true_ref = Referee(true_board, 'P1', 'P2')
     assert true_ref.check_for_tie() == True
 
 def test_check_for_draw_returns_false():
-    false_board = [1,10,1, 0,10,0, 1,0,10]
+    false_board = TableTop()
+    false_board.board = [1,10,1, 0,10,0, 1,0,10]
     false_ref = Referee(false_board, 'P1', 'P2')
     assert false_ref.check_for_tie() == False 
 
 def test_check_for_winner_returns_false():
-    false_board = [1,10,1, 0,10,0, 1,0,10]
+    false_board = TableTop()
+    false_board.board = [1,10,1, 0,10,0, 1,0,10]
     false_ref = Referee(false_board, 'P1', 'P2')
     assert false_ref.check_for_winner() == False 
 
 def test_check_for_winner_does_edges():
-    edge_board = [1,10,1, 1,10,0, 1,0,10]
+    edge_board = TableTop()
+    edge_board.board = [1,10,1, 1,10,0, 1,0,10]
     edge_ref = Referee(edge_board, 'P1', 'P2')
     assert edge_ref.check_for_winner() == True
 
 def test_check_for_winner_does_columns():
-    column_board = [1,10,1, 0,10,0, 1,10,0]
+    column_board = TableTop()
+    column_board.board = [1,10,1, 0,10,0, 1,10,0]
     column_ref = Referee(column_board, 'P1', 'P2')
     assert column_ref.check_for_winner() == True
 
 def test_check_for_winner_does_rows():
-    row_board = [1,0,1, 10,10,10 ,0,0,1]
+    row_board = TableTop()
+    row_board.board = [1,0,1, 10,10,10 ,0,0,1]
     row_ref = Referee(row_board, 'P1', 'P2')
     assert row_ref.check_for_winner() == True
 
 def test_check_for_winner_does_diagonals():
-    diagonal_board = [1,10,1, 10,1,0, 1,0,10]
+    diagonal_board = TableTop()
+    diagonal_board.board = [1,10,1, 10,1,0, 1,0,10]
     diagonal_ref = Referee(diagonal_board, 'P1', 'P2')
     assert diagonal_ref.check_for_winner() == True
 
 def test_get_board_size_3():
-    mock_3x3 = [0,0,0, 0,0,0, 0,0,0]
+    mock_3x3 = TableTop()
+    mock_3x3.board = [0,0,0, 0,0,0, 0,0,0]
     referee = Referee(mock_3x3, 'p1', 'p2')
-    assert referee.get_board_size(mock_3x3) == 3
+    assert referee.get_board_size(mock_3x3.board) == 3
 
 def test_get_board_size_4():
-    mock_4x4 = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]
+    mock_4x4 = TableTop()
+    mock_4x4.board = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]
     referee = Referee(mock_4x4, 'p1', 'p2')
-    assert referee.get_board_size(mock_4x4) == 4
+    assert referee.get_board_size(mock_4x4.board) == 4
 
 
