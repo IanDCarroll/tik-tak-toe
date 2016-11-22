61ea3612a0a0f367d360b4e48d0d8ad96a9d6c7a
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-21 23:00:08 -0800

    refactor test names

--- tests/referee_chair_test.py
+++ tests/referee_chair_test.py
@@ -7,45 +7,45 @@ def test_referee_is_an_object():
     assert isinstance(referee, object) == True
 
 def test_check_for_draw_returns_true():
-    true_board = TableTop()
-    true_board.board = [1,1,10, 10,10,1, 1,10,1]
-    true_ref = Referee(true_board, 'P1', 'P2')
+    true_top = TableTop()
+    true_top.board = [1,1,10, 10,10,1, 1,10,1]
+    true_ref = Referee(true_top, 'P1', 'P2')
     assert true_ref.check_for_tie() == True
 
 def test_check_for_draw_returns_false():
-    false_board = TableTop()
-    false_board.board = [1,10,1, 0,10,0, 1,0,10]
-    false_ref = Referee(false_board, 'P1', 'P2')
+    false_top = TableTop()
+    false_top.board = [1,10,1, 0,10,0, 1,0,10]
+    false_ref = Referee(false_top, 'P1', 'P2')
     assert false_ref.check_for_tie() == False 
 
 def test_check_for_winner_returns_false():
-    false_board = TableTop()
-    false_board.board = [1,10,1, 0,10,0, 1,0,10]
-    false_ref = Referee(false_board, 'P1', 'P2')
+    false_top = TableTop()
+    false_top.board = [1,10,1, 0,10,0, 1,0,10]
+    false_ref = Referee(false_top, 'P1', 'P2')
     assert false_ref.check_for_winner() == False 
 
 def test_check_for_winner_does_edges():
-    edge_board = TableTop()
-    edge_board.board = [1,10,1, 1,10,0, 1,0,10]
-    edge_ref = Referee(edge_board, 'P1', 'P2')
+    edge_top = TableTop()
+    edge_top.board = [1,10,1, 1,10,0, 1,0,10]
+    edge_ref = Referee(edge_top, 'P1', 'P2')
     assert edge_ref.check_for_winner() == True
 
 def test_check_for_winner_does_columns():
-    column_board = TableTop()
-    column_board.board = [1,10,1, 0,10,0, 1,10,0]
-    column_ref = Referee(column_board, 'P1', 'P2')
+    column_top = TableTop()
+    column_top.board = [1,10,1, 0,10,0, 1,10,0]
+    column_ref = Referee(column_top, 'P1', 'P2')
     assert column_ref.check_for_winner() == True
 
 def test_check_for_winner_does_rows():
-    row_board = TableTop()
-    row_board.board = [1,0,1, 10,10,10 ,0,0,1]
-    row_ref = Referee(row_board, 'P1', 'P2')
+    row_top = TableTop()
+    row_top.board = [1,0,1, 10,10,10 ,0,0,1]
+    row_ref = Referee(row_top, 'P1', 'P2')
     assert row_ref.check_for_winner() == True
 
 def test_check_for_winner_does_diagonals():
-    diagonal_board = TableTop()
-    diagonal_board.board = [1,10,1, 10,1,0, 1,0,10]
-    diagonal_ref = Referee(diagonal_board, 'P1', 'P2')
+    diagonal_top = TableTop()
+    diagonal_top.board = [1,10,1, 10,1,0, 1,0,10]
+    diagonal_ref = Referee(diagonal_top, 'P1', 'P2')
     assert diagonal_ref.check_for_winner() == True
 
 def test_get_board_size_3():
