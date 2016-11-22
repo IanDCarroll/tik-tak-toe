7b7767f6922526647281bbea1d4df767b944383c
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-21 23:09:37 -0800

    refactor referee to be less syntactically wrong

--- source/referee_chair.py
+++ source/referee_chair.py
@@ -5,35 +5,35 @@ import announcer_chair
 
 class Referee(Facilitator):
 
-    def __init__(self, tabletop, player1, player2):
-        self.tabletop = tabletop
+    def __init__(self, table_top, player1, player2):
+        self.table_top = table_top
         self.player1 = player1
         self.player2 = player2
         self.whos_turn = self.player1
 
     def start_game(self):
         announcer.show(announcer.start)
-        announcer.show(announcer.render_board(self.tabletop.board))
+        announcer.show(announcer.render_board(self.table_top.board))
         self.facilitate_turns()
 
     def facilitate_turns(self):
         self.whos_turn.move(self.board)
-        announcer.show(announcer.render_board(self.tabletop.board))
-        the_game_is_over = check_for_game_over(self.tabletop.board)
+        announcer.show(announcer.render_board(self.table_top.board))
+        the_game_is_over = check_for_game_over(self.table_top.board)
         if the_game_is_over != False:
             game_over(the_game_is_over)
         else:
-            prep_next_turn()
+            self.prep_next_turn()
+            self.facilitate_turns()
 
     def prep_next_turn(self):
         if self.whos_turn == self.player1:
             self.whos_turn = self.player2
         elif self.whos_turn == self.player2:
             self.whos_turn = self.player1
-        facilitate_turns()
         
     def game_over(self, winner):
-        announcer.show(announcer.render_board(self.tabletop.board))
+        announcer.show(announcer.render_board(self.table_top.board))
         if winner == "tie":
             announcer.show(display.draw)
         elif winner == "computer":
@@ -50,13 +50,13 @@ class Referee(Facilitator):
             return False
 
     def check_for_tie(self):
-        if 0 in self.tabletop.board:
+        if 0 in self.table_top.board:
             return False
         else:
             return True
 
     def check_for_winner(self):
-        board = self.tabletop.board
+        board = self.table_top.board
         scan_list = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],
                      [1,4,7],[2,5,8],[0,4,8],[2,4,6]]
         did_they_win = False
