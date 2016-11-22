86dfb99a2fccfcbb89a761d9e9d3280bd6d02b28
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-21 21:22:36 -0800

    have 2 classes with the same method

--- source/game_table.py
+++ source/game_table.py
@@ -4,6 +4,3 @@ class TableTop(object):
     
     def __init__(self):
         self.board = [0,0,0,0,0,0,0,0,0]
-
-    def get_board_size(self, board):
-        return int(sqrt(len(board))) 
--- tests/game_table_test.py
+++ tests/game_table_test.py
@@ -15,14 +15,4 @@ def test_that_board_can_be_changed():
     table_top = TableTop()
     expected = [0,0,1,0,0,0,0,0,0]
     table_top.board = expected
-    assert table_top.board == expected
-
-def test_get_board_size_3():
-    mock_3x3 = [0,0,0, 0,0,0, 0,0,0]
-    table_top = TableTop()
-    assert table_top.get_board_size(mock_3x3) == 3
-
-def test_get_board_size_4():
-    mock_4x4 = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]
-    table_top = TableTop()
-    assert table_top.get_board_size(mock_4x4) == 4
+    assert table_top.board == expected 
