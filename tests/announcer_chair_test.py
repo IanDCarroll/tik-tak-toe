01287cc3d0ac2b0e2e1210ada9521ab1dfb525bf
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-21 22:33:38 -0800

    tidy up announcer tests

--- tests/announcer_chair_test.py
+++ tests/announcer_chair_test.py
@@ -2,29 +2,29 @@ import nose.tools
 from source.announcer_chair import *
 
 def test_display_start_of_game():
-    display = Announcer()
-    test = display.start
+    announcer = Announcer()
+    test = announcer.start
     assert test == "Welcome"
 
 def test_display_game_over_draw():
-    display = Announcer()
-    test = display.draw
+    announcer = Announcer()
+    test = announcer.draw
     assert test == "Draw"
 
 def test_display_game_over_computer_wins():
-    display = Announcer()
-    test = display.computer
+    announcer = Announcer()
+    test = announcer.computer
     assert test == "Computer Wins"
 
 def test_display_game_over_human_wins():
-    display = Announcer()
-    test = display.human
+    announcer = Announcer()
+    test = announcer.human
     assert test == "You Win"
 
 def test_display_current_board():
-    display = Announcer()
+    announcer = Announcer()
     mock_board = [1,10,1, 0,10,0, 1,0,10]
-    test = display.render_board(mock_board)
+    test = announcer.render_board(mock_board)
     expected = """
  X | O | X 
 ---+---+---
@@ -35,11 +35,11 @@ def test_display_current_board():
     assert test == expected
 
 def test_display_ask_for_human_input():
-    display = Announcer()
-    test = display.next_move
+    announcer = Announcer()
+    test = announcer.next_move
     assert test == "What is your next move?"
 
 def test_display_human_made_illegal_move():
-    display = Announcer()
-    test = display.bad_move
+    announcer = Announcer()
+    test = announcer.bad_move
     assert test == "That is not a legal move."
