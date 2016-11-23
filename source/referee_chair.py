9e1598a5e6cff2af8d1d580c1de617e9bfdeba34
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-22 20:26:15 -0800

    replace win_list to reduce test noise

--- source/referee_chair.py
+++ source/referee_chair.py
@@ -62,11 +62,12 @@ class Referee(Facilitator):
 
     def check_for_winner(self):
         board = self.table_top.board
-        win_list = self.get_win_list()
+        win_list = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],
+                    [1,4,7],[2,5,8],[0,4,8],[2,4,6]]
         did_they_win = False
-        for i in range(0, len(scan_list)):
+        for i in range(0, len(win_list)):
             win_factor = 0
-            for j in scan_list[i]:
+            for j in win_list[i]:
                 win_factor += board[j]
             if win_factor == 3 or win_factor == 30:
                 did_they_win = True
