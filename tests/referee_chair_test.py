1fb15e60fc61d94e22a4a14e09662bc0e0650651
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-22 22:03:01 -0800

    prove get_win_list can handle all 2D squares

--- tests/referee_chair_test.py
+++ tests/referee_chair_test.py
@@ -77,3 +77,12 @@ def test_get_win_list_returns_the_3x3_wins():
     expected = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],
                 [1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     assert ref_3x3.get_win_list() == expected
+
+def test_get_win_list_returns_the_4x4_wins():
+    top_4x4 = TableTop()
+    top_4x4.board = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]
+    expected = [[0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15],
+                [0,4,8,12], [1,5,9,13], [2,6,10,14], [3,7,11,15],
+                [0,5,10,15],[3,6,9,12]]
+    ref_4x4 = Referee(top_4x4, 'P1', 'P2')
+    assert ref_4x4.get_win_list() == expected
