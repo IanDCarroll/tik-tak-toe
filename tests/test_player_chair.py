876d61fe3bbef371dddb4e2ec01543a6bbcc3d57
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-26 22:26:49 -0800

    add setUp() method to player tests

--- tests/test_player_chair.py
+++ tests/test_player_chair.py
@@ -3,21 +3,24 @@ from source.player_chair import *
 
 class PlayerTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.mock_board = [1,10,1, 0,10,0, 1,0,10]
+
     def test_that_player_can_only_make_legal_moves(self):
         philip = Player()
-        mock_board = [1,10,1, 10,0,0, 1,0,10]
+        mock_board = self.mock_board
         test = philip.get_legal_moves(mock_board)
-        self.assertEqual(test, [4,5,7])
+        self.assertEqual(test, [3,5,7])
 
     def test_that_computer_player_can_make_a_move(self):
         spamBot9000 = Computer()
-        mock_board = [1,10,1, 0,10,0, 1,0,10]
+        mock_board = self.mock_board
         test = spamBot9000.move(mock_board)
         self.assertEqual(test, [1,10,1, 1,10,0, 1,0,10])
 
     def test_that_human_can_only_make_legal_moves(self):
         bernice = Human()
-        mock_board = [1,10,1, 0,10,0, 1,0,10]
+        mock_board = self.mock_board
         test_true = bernice.check_conscience(7, mock_board)
         self.assertEqual(test_true, None)
 
