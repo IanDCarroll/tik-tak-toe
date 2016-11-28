5d328d540e86bea748468000a1d7bcd6e9e96ad0
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-27 19:15:01 -0800

    refactor tests in test_game_table.py

--- tests/test_game_table.py
+++ tests/test_game_table.py
@@ -4,24 +4,22 @@ from source.game_table import *
 class TableTopTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.table_top = TableTop()
         self.empty_board = [0,0,0, 0,0,0, 0,0,0]
         self.changed_board = [0,0,1, 0,0,0, 0,0,0] 
 
     def test_that_board_returns_a_list(self):
-        table_top = TableTop()
-        test = isinstance(table_top.board, list)
+        test = isinstance(self.table_top.board, list)
         self.assertEqual(test, True)
 
     def test_the_boards_initial_state(self):
-        table_top = TableTop()
         expected = self.empty_board
-        self.assertEqual(table_top.board, expected)
+        self.assertEqual(self.table_top.board, expected)
 
     def test_that_board_can_be_changed(self):
-        table_top = TableTop()
         expected = self.changed_board
-        table_top.board = expected
-        self.assertEqual(table_top.board, expected)
+        self.table_top.board = self.changed_board
+        self.assertEqual(self.table_top.board, expected)
 
 if __name__ == '__main__':
     unittest.main()
