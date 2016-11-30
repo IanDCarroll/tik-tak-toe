66ed2ee48c64de3c9853012b09ef46bd43052e20
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-30 09:48:58 -0800

    delete unnnecessary code

--- source/referee_chair.py
+++ source/referee_chair.py
@@ -38,7 +38,7 @@ class Referee(Facilitator):
         self.show_board()
         self.whos_turn.move(self.table_top.board)
         self.moves_taken += 1
-        sleep(0.5)
+        sleep(0.75)
         the_game_is_over = self.check_for_game_over()
         if the_game_is_over != False:
             self.game_over(the_game_is_over)
@@ -79,71 +79,10 @@ class Referee(Facilitator):
 
     def check_for_winner(self):
         board = self.table_top.board
-        p1_win = self.get_board_size(board)
-        p2_win = p1_win * 10
-        win_list = self.get_win_list()
-        did_they_win = False
-        for i in range(0, len(win_list)):
-            win_factor = 0
-            for j in win_list[i]:
-                win_factor += board[j]
-            if win_factor == p1_win or win_factor == p2_win:
-                did_they_win = True
-        return did_they_win
-
-    def get_win_list(self):
-        board = self.table_top.board
-        board_size = self.get_board_size(board)
-        win_list = []
-        win_list.extend(self.get_winning_rows(board_size))
-        win_list.extend(self.get_winning_cols(board_size))
-        win_list.extend(self.get_winning_diags(board_size))
-        return win_list
-
-    def get_winning_rows(self, board_size):
-        winning_rows = self.get_empty_list(board_size)
-        start_coord = 0
-        for i in range(0, board_size):
-            stop_coord = start_coord + board_size
-            for j in range(start_coord, stop_coord):
-                 winning_rows[i].append(j)
-            start_coord += board_size
-        return winning_rows
-
-    def get_winning_cols(self, board_size):
-        winning_cols = self.get_empty_list(board_size)
-        start_coord = 0
-        stop_coord = board_size * board_size
-        for i in range(0, board_size):
-            for j in range(start_coord, stop_coord, board_size):
-                 winning_cols[i].append(j)
-            start_coord += 1
-        return winning_cols
- 
-    def get_empty_list(self, board_size):
-        empty_list = []
-        for i in range(0, board_size):
-            empty_list.append([])
-        return empty_list
-
-    def get_winning_diags(self, board_size):
-        winning_diags = []
-        winning_diags.append(self.get_NW_SE_diag(board_size))
-        winning_diags.append(self.get_SW_NE_diag(board_size))
-        return winning_diags
-
-    def get_NW_SE_diag(self, board_size):
-        diag = []
-        coord = 0
-        for i in range(0, board_size):
-            diag.append(coord)
-            coord += board_size + 1
-        return diag
-
-    def get_SW_NE_diag(self, board_size):
-        diag = []
-        coord = board_size -1
-        for i in range(0, board_size):
-            diag.append(coord)
-            coord += board_size - 1
-        return diag
+        crosses_win = self.get_board_size(board)
+        noughts_win = crosses_win * 10
+        scan = self.scan_board(board)
+        if crosses_win in scan or noughts_win in scan:
+            return True
+        else:
+            return False
