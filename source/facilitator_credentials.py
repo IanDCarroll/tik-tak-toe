df93eb4220e143158014c776fc54d88d738928e4
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-30 09:03:47 -0800

    write scan_cols to return an analyzed list

--- source/facilitator_credentials.py
+++ source/facilitator_credentials.py
@@ -5,6 +5,7 @@ class Facilitator(object):
     def scan_board(self, board):
         analyzed_list = []
         analyzed_list.extend(self.scan_rows(board))
+        analyzed_list.extend(self.scan_cols(board))
         return analyzed_list
 
     def scan_rows(self, board):
@@ -19,6 +20,19 @@ class Facilitator(object):
             start_index += board_size
             analyzed_rows.append(analyzed_row)
         return analyzed_rows
+
+    def scan_cols(self, board):
+        analyzed_cols = []
+        start_index = 0
+        board_size = self.get_board_size(board)
+        stop_index = len(board)
+        for i in range(0, board_size):
+            analyzed_col = 0
+            for j in range(start_index, stop_index, board_size):
+                analyzed_col += board[j]
+            start_index += 1
+            analyzed_cols.append(analyzed_col)
+        return analyzed_cols
 
     def get_board_size(self, board):
         return int(sqrt(len(board))) 
