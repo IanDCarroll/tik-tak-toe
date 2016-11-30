e7bdd573c9b19c4afa535c981d14303115832391
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-30 08:34:04 -0800

    test that scan rows returns analyzed rows

--- test_facilitator.py
+++ test_facilitator.py
@@ -7,10 +7,15 @@ class FacilitatorTestCase(unittest.TestCase):
         self.facilitator = Facilitator()
         self.mock_board = [1,0,10, 1,10,0, 1,10,1]
         self.expected_analysis = [11,11,12,3,20,11,12,21]
+        self.expected_rows = [11,11,12]
 
     def test_scan_board_returns_analyzed_list(self):
         test_yields = self.facilitator.scan_board(self.mock_board)
         self.assertEqual(test_yields, self.expected_analysis)
+
+    def test_scan_rows_returns_analyzed_list(self):
+        test_yields = self.facilitator.scan_rows(self.mock_board)
+        self.assertEqual(test_yields, self.expected_rows)
 
 if __name__ == '__main__':
     unittest.main()
