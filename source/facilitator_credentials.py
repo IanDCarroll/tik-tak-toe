abfcc119dabb79267db0fe5efb5028b8cc4545bb
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-30 08:44:24 -0800

    write scan_rows to return an analyzed row

--- source/facilitator_credentials.py
+++ source/facilitator_credentials.py
@@ -3,7 +3,22 @@ from math import sqrt
 class Facilitator(object):
 
     def scan_board(self, board):
-        pass
+        analyzed_list = []
+        analyzed_list.extend(self.scan_rows(board))
+        return analyzed_list
+
+    def scan_rows(self, board):
+        analyzed_rows = []
+        board_size = self.get_board_size(board)
+        start_index = 0
+        for i in range(0, board_size):
+            stop_index = start_index + board_size
+            analyzed_row = 0
+            for j in range(start_index, stop_index):
+                analyzed_row += board[j]
+            start_index += board_size
+            analyzed_rows.append(analyzed_row)
+        return analyzed_rows
 
     def get_board_size(self, board):
         return int(sqrt(len(board))) 
