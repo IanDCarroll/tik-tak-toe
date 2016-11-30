62091e125f7c728620f66530a38ad3e12424aef4
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-30 08:24:07 -0800

    fix errors in test

--- test_facilitator.py
+++ test_facilitator.py
@@ -1,12 +1,12 @@
 import unittest
-from facilitator_credentials import *
+from source.facilitator_credentials import *
 
-class FacilitatorTestCase(Unittest.TestCase):
+class FacilitatorTestCase(unittest.TestCase):
 
     def setUp(self):
-        faclilitator = Facilitator()
-        mock_board = [1,0,10, 1,10,0, 1,10,1]
-        expected_analysis = [11,11,12,3,20,11,12,21]
+        self.facilitator = Facilitator()
+        self.mock_board = [1,0,10, 1,10,0, 1,10,1]
+        self.expected_analysis = [11,11,12,3,20,11,12,21]
 
     def test_scan_board_returns_analyzed_list(self):
         test_yields = self.facilitator.scan_board(self.mock_board)
