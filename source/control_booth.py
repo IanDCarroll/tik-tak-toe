f476a10b327c53af9fb9ef28ccc162924a470857
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-01 22:01:12 -0800

    import a different referee to run the mid-game

--- source/control_booth.py
+++ source/control_booth.py
@@ -1,8 +1,12 @@
 from emcee_podium import Emcee as mc
+from referee_2_chair import Referee
 
 class StageManager(object):
 
     def play_game(self):
         play_options = mc.open_game()
-        winner = ref.run_game(play_options)
+
+        ref = Referee(play_options)
+        winner = ref.run_game()
+
         return mc.end_game(winner)      
