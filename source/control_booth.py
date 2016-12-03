d214ad0585333b5b316909d5357d5500dd184365
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-02 22:34:12 -0800

    refactor for a better abstraction

--- source/control_booth.py
+++ source/control_booth.py
@@ -1,4 +1,4 @@
-from emcee_podium import Emcee as mc
+from emcee_podium import Emcee
 from referee_2_chair import Referee
 from game_table import TableTop
 
@@ -8,9 +8,8 @@ class StageManager(object):
         self.table_top = TableTop()
 
     def play_game(self):
-        game_mode = mc.open_game()
-        if game_mode == '2':
-            self.table_top.give_computer_the_first_move()
+        mc = Emcee(table_top)
+        mc.choose_who_goes_first()
 
         ref = Referee(table_top)
         winner = ref.facilitate_turns()
--- source/emcee_podium.py
+++ source/emcee_podium.py
@@ -4,9 +4,14 @@ class Emcee(object):
 
     announcer = Announcer()
 
-    def open_game(self):
+    def choose_who_goes_first(self):
         self.announcer.show(self.announcer.start)
         self.announcer.show(self.announcer.select)
+        selection = get_selection()
+        if selection = '2':
+           self.table_top.give_computer_the_first_move()
+
+    def get_selection(self):
         return self.ask_human()
         if select == '1' or select == '2':
             return select
