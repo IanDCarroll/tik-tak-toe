69b7dd5355ea34cbe76e378356a779c27397bcde
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-02 22:11:11 -0800

    give moves_taken back to referee

--- source/game_table.py
+++ source/game_table.py
@@ -9,7 +9,6 @@ class TableTop(object):
         self.player1 = Human(self.crosses)
         self.player2 = Computer(self.noughts)
         self.whos_turn = self.player1
-        self.moves_taken = 0
 
     def switch_players(self):
         self.player1 = Computer(self.crosses)
