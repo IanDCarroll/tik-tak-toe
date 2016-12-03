9c84660ae7ca92efe6cc561836cb5ee6127737f8
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-02 22:36:34 -0800

    fix syntax error

--- source/emcee_podium.py
+++ source/emcee_podium.py
@@ -8,7 +8,7 @@ class Emcee(object):
         self.announcer.show(self.announcer.start)
         self.announcer.show(self.announcer.select)
         selection = get_selection()
-        if selection = '2':
+        if selection == '2':
            self.table_top.give_computer_the_first_move()
 
     def get_selection(self):
