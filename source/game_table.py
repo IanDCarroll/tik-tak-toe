58463482d2b3f7496ef60287908bd326cb8c29f1
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-02 21:13:19 -0800

    give TableTop referee's instance variables

--- source/game_table.py
+++ source/game_table.py
@@ -2,3 +2,9 @@ class TableTop(object):
     
     def __init__(self):
         self.board = [0,0,0, 0,0,0, 0,0,0]
+        self.noughts = 10
+        self.crosses = 1
+        self.player1 = Human(self.crosses)
+        self.player2 = Computer(self.noughts)
+        self.whos_turn = self.player1
+        self.moves_taken = 0
