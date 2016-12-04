babc4e95e2aef8552661496a8168e5757277267d
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 19:59:22 -0800

    alter Judges init to accept the table_top

--- source/judge_pit.py
+++ source/judge_pit.py
@@ -2,5 +2,8 @@ from observer_abilities import *
 
 class Judge(Observer):
 
+    def __init__(self, board_object):
+       self.table_top = board_object
+
     def check_for_winner(self):
         pass
