c5301385f2e4f4f22d8789a549ac7be0e024c3b3
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 22:28:00 -0800

    assert check for winner returns p2 human

--- tests/test_judge_pit.py
+++ tests/test_judge_pit.py
@@ -9,7 +9,7 @@ class JudgeTestCase(unittest.TestCase):
         self.judge = Judge(self.table_top)
         self.mock_no_win_board = [1,10,1, 0,10,0, 1,0,10]
         self.mock_p1_win_board = [1,10,1, 1,10,0, 1,0,10]
-
+        self.mock_p2_win_board = [10,1,10, 1,10,1, 10,0,1]
 
     def test_check_for_winner_returns_no_winner(self):
         self.table_top.board = self.mock_no_win_board
@@ -17,13 +17,17 @@ class JudgeTestCase(unittest.TestCase):
         self.assertEqual(test_yields, False)
 
     def test_check_for_winner_returns_computer_p1_win(self):
-        self.table_top.board = self.mock_p1_win_board
         self.table_top.give_computer_the_first_move()
+        self.table_top.board = self.mock_p1_win_board
         test_yields = self.judge.check_for_winner()
         self.assertEqual(test_yields, 'computer')
 
     def test_check_for_winner_returns_computer_p2_win(self):
-        pass
+        self.table_top.give_computer_the_first_move()
+        self.table_top.give_next_player_a_go()
+        self.table_top.board = self.mock_p2_win_board
+        test_yields = self.judge.check_for_winner()
+        self.assertEqual(test_yields, 'human')
 
     def test_check_for_winner_returns_human_p1_win(self):
         pass
