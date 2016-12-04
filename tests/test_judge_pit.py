3bed5f78018d3bc1eeeb9a46b860e82aec640429
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 20:07:59 -0800

    write setup and new test skeleton for judge

--- tests/test_judge_pit.py
+++ tests/test_judge_pit.py
@@ -3,6 +3,13 @@ from source.judge_pit import *
 class JudgeTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.human_1st_board = TableTop()
+        self.computer_1st_board = TableTop()
+        self.computer_1st_board.give_computer_the_first_move()
+        self.human_1st_judge = Judge(self.human_1st_board)
+        self.computer_1st_judge = Judge(self.computer_1st_board)
+
+    def test_check_for_winner_returns_no_winner(self):
         pass
 
     def test_check_for_winner_returns_computer_p1_win(self):
