638116af0bf72e73caae64ad36efc74f385dc09e
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 22:01:16 -0800

    check for winner retuns false if no winner

--- source/judge_pit.py
+++ source/judge_pit.py
@@ -6,4 +6,11 @@ class Judge(Observer):
        self.table_top = board_object
 
     def check_for_winner(self):
-        pass
+        board = self.table_top.board
+        crosses_win = self.get_board_size(board)
+        noughts_win = crosses_win * 10
+        scan = self.scan_board(board)
+        if crosses_win in scan or noughts_win in scan:
+           return True
+        else:
+           return False
