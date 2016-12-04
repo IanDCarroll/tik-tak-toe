588b6e1d01e6e5ea2dcf961ae1a53b415b909a87
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 19:22:38 -0800

    test that the right players are at the table

--- tests/test_emcee_podium.py
+++ tests/test_emcee_podium.py
@@ -1,13 +1,17 @@
 import unittest
 from source.emcee_podium import *
 from source.announcer_chair import *
+from source.game_table import *
 
 class MuteAnnouncer(Announcer):
     def show(self, what_is_said):
         return what_is_said
 
 class Dummy(Emcee):
-    announcer = MuteAnnouncer()
+
+    def __init__(self, board):
+        self.table_top = board
+        self.announcer = MuteAnnouncer()
 
 class Mc_Human(Dummy):
     def ask_human(self):
@@ -24,14 +28,41 @@ class Mc_Error(Dummy):
 class EmceeTestCase(unittest.TestCase):
 
     def setUp(self):
+        self.table_top = TableTop()
         self.signer = MuteAnnouncer()
-        self.mc_dummy = Dummy()
-        self.mc_human = Mc_Human()
-        self.mc_computer = Mc_Computer()
-        self.mc_error = Mc_Error()
+        self.mc_dummy = Dummy(self.table_top)
+        self.mc_human = Mc_Human(self.table_top)
+        self.mc_computer = Mc_Computer(self.table_top)
+        self.mc_error = Mc_Error(self.table_top)
+        self.a_human_p1 = Human(1)
+        self.a_human_p2 = Human(10)
+        self.a_computer_p1 = Computer(1)
+        self.a_computer_p2 = Computer(10)
         self.human_win = 'human'
         self.computer_win = 'computer'
         self.tied_game = 'tie'
+
+    def test_choose_who_goes_first_doesnt_change_players(self):
+        self.mc_human.choose_who_goes_first()
+        p1_name = self.mc_human.table_top.player1.name
+        p1_code = self.mc_human.table_top.player1.marker_code
+        p2_name = self.mc_human.table_top.player2.name
+        p2_code = self.mc_human.table_top.player2.marker_code
+        self.assertEqual(p1_name, self.a_human_p1.name)
+        self.assertEqual(p1_code, self.a_human_p1.marker_code)
+        self.assertEqual(p2_name, self.a_computer_p2.name)
+        self.assertEqual(p2_code, self.a_computer_p2.marker_code)
+
+    def test_choose_who_goes_first_changes_players(self):
+        self.mc_computer.choose_who_goes_first()
+        p1_name = self.mc_computer.table_top.player1.name
+        p1_code = self.mc_computer.table_top.player1.marker_code
+        p2_name = self.mc_computer.table_top.player2.name
+        p2_code = self.mc_computer.table_top.player2.marker_code
+        self.assertEqual(p1_name, self.a_computer_p1.name)
+        self.assertEqual(p1_code, self.a_computer_p1.marker_code)
+        self.assertEqual(p2_name, self.a_human_p2.name)
+        self.assertEqual(p2_code, self.a_human_p2.marker_code)
 
     def test_get_choice_human_chooses_to_go_first(self):
         self.assertEqual(self.mc_human.get_choice(), '1')
