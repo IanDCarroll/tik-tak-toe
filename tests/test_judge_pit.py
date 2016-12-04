1cd42156db1ed90c8fdb7d86c3577dad659d5399
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 19:53:02 -0800

    add testing skeleton for Judge

---
+++ tests/test_judge_pit.py
@@ -0,0 +1,18 @@
+from source.judge_pit import *
+
+class JudgeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_check_for_winner_returns_computer_p1_win(self):
+        pass
+
+    def test_check_for_winner_returns_computer_p2_win(self):
+        pass
+
+    def test_check_for_winner_returns_human_p1_win(self):
+        pass
+
+    def test_check_for_winner_returns_human_p2_win(self):
+        pass
