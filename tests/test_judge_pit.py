f11a16da10f82070b9c515117b0f61db5852c361
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 22:30:29 -0800

    assert check_for_winner yeilds human if human wins

--- tests/test_judge_pit.py
+++ tests/test_judge_pit.py
@@ -30,7 +30,9 @@ class JudgeTestCase(unittest.TestCase):
         self.assertEqual(test_yields, 'human')
 
     def test_check_for_winner_returns_human_p1_win(self):
-        pass
+        self.table_top.board = self.mock_p1_win_board
+        test_yields = self.judge.check_for_winner()
+        self.assertEqual(test_yields, 'human')
 
     def test_check_for_winner_returns_human_p2_win(self):
         pass
