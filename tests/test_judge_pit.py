6e8026e56637dce647e3435c596ddfd59b15986b
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-03 21:55:59 -0800

    test that a non-winning board returns false

--- tests/test_judge_pit.py
+++ tests/test_judge_pit.py
@@ -1,4 +1,6 @@
+import unittest
 from source.judge_pit import *
+from source.game_table import *
 
 class JudgeTestCase(unittest.TestCase):
 
@@ -8,9 +10,12 @@ class JudgeTestCase(unittest.TestCase):
         self.computer_1st_board.give_computer_the_first_move()
         self.human_1st_judge = Judge(self.human_1st_board)
         self.computer_1st_judge = Judge(self.computer_1st_board)
+        self.mock_no_win_board = [1,10,1, 0,10,0, 1,0,10]
 
     def test_check_for_winner_returns_no_winner(self):
-        pass
+        self.human_1st_board.board = self.mock_no_win_board
+        test_yields = self.human_1st_judge.check_for_winner()
+        self.assertEqual(test_yields, False)
 
     def test_check_for_winner_returns_computer_p1_win(self):
         pass
