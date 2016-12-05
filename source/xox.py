333802d663df101f2d9ee96f875fa826955a7997
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-04 21:27:27 -0800

    switch out old Referee for new Stage Manager

--- source/xox.py
+++ source/xox.py
@@ -1,10 +1,5 @@
-from referee_chair import Referee
-from game_table import TableTop
-
-def play_xox():
-    tabletop = TableTop()
-    ref = Referee(tabletop)
-    ref.start_game()
+from control_booth import *
 
 if __name__ == "__main__":
-    play_xox()
+    xox = StageManager()
+    xox.play_game()
