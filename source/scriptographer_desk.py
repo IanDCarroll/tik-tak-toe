d2d6a2f296fdf58e47125e1c480b42d9378f1625
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-05 12:28:57 -0800

    add single place where print resources are kept

---
+++ source/scriptographer_desk.py
@@ -0,0 +1,17 @@
+class Scriptographer(object):
+
+    def __init__(self):
+        self.start = "Welcome to XOX, a Noughts and Crosses Game"
+        self.select = "Type 1 to go first, or 2 to go second."
+        self.tie = "The game is a draw."
+        self.computer = "The Computer wins the game."
+        self.human = "You Win!"
+        self.question = "Which square do you choose?"
+        self.bad_move = "Sorry, that's not a legal move. Try again."
+        self.nought = "\033[34m O \033[0m"
+        self.cross = "\033[91m X \033[0m"
+        self.before_num = "\033[30m "
+        self.after_num = " \033[0m"
+        self.plank = '---'
+        self.corner = '+'
+        self.wall = '|'
