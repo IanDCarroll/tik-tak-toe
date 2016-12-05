251e88398541124555b0c87edf83a59c4ea0347a
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-05 13:12:49 -0800

    add testing framework for new announcer

---
+++ tests/test_announcer_2_chair.py
@@ -0,0 +1,16 @@
+import unittest
+from source.announcer_2_chair import *
+
+class AnnouncerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_announcer_is_a_class(self):
+        pass
+
+    def test_announcer_has_a_show_method(self):
+        pass
+
+    def test_announcer_has_an_ask_human_method(self):
+        pass
