2778e0a06667957c7f3aea79a2e7e0fd436a72c4
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-05 13:19:34 -0800

    write first test case

--- tests/test_announcer_2_chair.py
+++ tests/test_announcer_2_chair.py
@@ -4,10 +4,11 @@ from source.announcer_2_chair import *
 class AnnouncerTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.announcer = Announcer()
+        self.methodList = [method for method in dir(self.announcer) if callable(getattr(self.announcer, method))]
 
     def test_announcer_is_a_class(self):
-        pass
+        self.assertIsInstance(self.announcer, Announcer)
 
     def test_announcer_has_a_show_method(self):
         pass
