5acef5ea7ac5816845807a5bee4edfa3cbdda689
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-04 20:13:56 -0800

    pass test for take_a_turn()

--- source/referee_2_chair.py
+++ source/referee_2_chair.py
@@ -9,19 +9,20 @@ class Referee(object):
         self.moves_taken = 0
 
     def facilitate_turns(self):
+        self.show_board()
         winner = self.judge.check_for_winner()
         if winner:
            return winner
-        elif moves_taken == len(self.table_top.board): 
+        elif moves_taken == len(self.table_top.board):
            return 'tie'
         else:
            self.take_a_turn()
+           return self.facilitate_turns()
 
     def take_a_turn(self):
-        self.moves_taken += 1
-        self.show_board()
-        self.table_top.give_next_player_a_go()
         self.table_top.whos_turn.move(self.table_top.board)
+        self.moves_taken += 1
+        self.table_top.give_next_player_a_go()
 
     def show_board(self):
         board = self.announcer.render_board(self.table_top.board)
