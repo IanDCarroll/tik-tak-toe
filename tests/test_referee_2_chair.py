8ee168a4730248200c2999edc028c98ab4d2c42e
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-04 19:13:35 -0800

    add test skeleton for the new referee

---
+++ tests/test_referee_2_chair.py
@@ -0,0 +1,22 @@
+import unittest
+from source.referee_2_chair import *
+
+class RefereeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_referee_ties_when_all_squares_taken_no_winner(self):
+        pass
+
+    def test_referee_returns_win_even_if_all_squares_taken(self):
+        pass
+
+    def test_referee_does_what_it_should_if_no_tie_or_win(self):
+        pass
+
+    def test_referee_changes_the_board_when_it_takes_a_turn(self):
+        pass
+
+    def test_show_board_shows_the_board(self):
+        pass
