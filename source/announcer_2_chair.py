556a990f56f8c8161234b00f8c16b5d503ca7c37
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-05 12:41:52 -0800

    add new announcer class

---
+++ source/announcer_2_chair.py
@@ -0,0 +1,9 @@
+from scriptographer.desk import *
+
+class Announcer(Scriptographer):
+
+    def show(self, text):
+        print text
+
+    def ask_human(self):
+        return raw_input(self.prompt)
