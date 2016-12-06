7a669705a4870f04dfee620f24fb98d60b31b901
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-06 13:31:05 -0800

    add initial skeleton for carpenter tests

---
+++ tests/test_carpenter_shop.py
@@ -0,0 +1,10 @@
+import unittest
+from source.carpenter_shop import *
+
+class CarpenterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
