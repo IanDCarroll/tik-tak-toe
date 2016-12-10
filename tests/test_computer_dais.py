d4152ae5bc4033da598ca58a85a013e77447d7fc
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-10 10:07:07 -0800

    modify tests to accept options instead of boards

--- tests/test_computer_dais.py
+++ tests/test_computer_dais.py
@@ -5,24 +5,26 @@ class ComputerTestCase(unittest.TestCase):
 
     def setUp(self):
         self.hal = Computer(1)
+        self.empty_board = [0,0,0, 0,0,0, 0,0,0]
         self.hollow_board = [1,10,0, 0,0,0, 10,1,0]
         self.center_board = [10,0,0, 0,1,0, 10,0,1]
         self.diagon_board = [10,0,1, 1,1,10, 10,0,1]
 
+        self.fill_center = [0,0,0, 0,1,0, 0,0,0]
         self.fill_hollows = [1,10,0, 0,1,0, 10,1,0]
         self.catty_corner = [0,0,10, 0,1,0, 1,0,0]
         self.fill_corners = [10,0,1, 0,1,0, 10,0,1]
         self.fill_whatevs = [10,1,1, 1,1,10, 10,0,1]
 
-        self.open_4 = [0,0,0, 0,0,0, 0,0,0]
-        self.open_0 = [0,0,0, 0,1,0, 0,0,0]
-        self.open_2 = [1,0,0, 0,1,0, 0,0,0]
-        self.open_6 = [1,0,1, 0,1,0, 0,0,0]
-        self.open_8 = [1,0,1, 0,1,0, 1,0,0]
-        self.open_1 = [1,0,1, 0,1,0, 1,0,1]
-        self.open_3 = [1,1,1, 0,1,0, 1,0,1]
-        self.open_5 = [1,1,1, 1,1,0, 1,0,1]
-        self.open_7 = [1,1,1, 1,1,1, 1,0,1]
+        self.open_4 = [0,1,2, 3,4,5, 6,7,8]
+        self.open_0 = [0,1,2, 3,  5, 6,7,8]
+        self.open_2 = [  1,2, 3,  5, 6,7,8]
+        self.open_6 = [  1,   3,  5, 6,7,8]
+        self.open_8 = [  1,   3,  5,   7,8]
+        self.open_1 = [  1,   3,  5,   7  ]
+        self.open_3 = [       3,  5,   7  ]
+        self.open_5 = [           5,   7  ]
+        self.open_7 = [                7  ]
 
         self.NW_corner_x = [10,0,0, 0,1,0, 0,0,0]
         self.NW_corner_o = [1,0,0, 0,10,0, 0,0,0]
@@ -31,8 +33,8 @@ class ComputerTestCase(unittest.TestCase):
         self.SE_corner = [0,0,0, 0,1,0, 0,0,10]
 
     def test_computer_prefers_the_center_to_all_else(self):
-        test_yields = self.hal.move(self.open_4)
-        self.assertEqual(test_yields, self.open_0)
+        test_yields = self.hal.move(self.empty_board)
+        self.assertEqual(test_yields, self.fill_center)
         test_yields = self.hal.move(self.hollow_board)
         self.assertEqual(test_yields, self.fill_hollows)
 
