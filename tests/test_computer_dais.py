043dbcc253b32c6be244e5ebfd4d12097e1e9f5f
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-09 21:53:44 -0800

    assert that the computer perfers the center

--- tests/test_computer_dais.py
+++ tests/test_computer_dais.py
@@ -4,7 +4,7 @@ from OnStage.computer_dais import *
 class ComputerTestCase(unittest.TestCase):
 
     def setUp(self):
-        hal = Computer(1)
+        self.hal = Computer(1)
         self.empty_board = [0,0,0, 0,0,0, 0,0,0]
         self.hollow_board = [1,10,0, 0,0,0, 10,1,0]
         self.center_board = [10,0,0, 0,1,0, 10,0,1]
@@ -16,7 +16,10 @@ class ComputerTestCase(unittest.TestCase):
         self.fill_whatevs = [10,1,1, 1,1,10, 10,0,1]
 
     def test_the_computer_prefers_the_center_to_all_else(self):
-        pass
+        test_yields = self.hal.move(self.empty_board)
+        self.assertEqual(test_yields, self.take_center)
+        test_yields = self.hal.move(self.hollow_board)
+        self.assertEqual(test_yields, self.fill_hollows)
 
     def test_the_computer_prefers_corners_to_edges(self):
         pass
