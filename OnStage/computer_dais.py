ae488e998292b7eddf7840f2c8783c6e6085a782
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-10 09:41:05 -0800

    create framework for take_catty_corner

--- OnStage/computer_dais.py
+++ OnStage/computer_dais.py
@@ -6,14 +6,20 @@ class Computer(Player):
 
     def choose(self, board):
         center = self.take_the_center(board)
+        catty_corner = self.take_catty_corner(board)
         if center:
              return center
+        if catty_corner:
+             return catty_corner
         return self.make_default_choice(board)
 
     def take_the_center(self, board):
         options = self.get_legal_moves(board)
         if 4 in options:
             return 4
+        return False
+
+    def take_catty_corner(self, board):
         return False
 
     def make_default_choice(self, board):
