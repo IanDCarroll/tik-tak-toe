0ed737c9d728bd3240771723e6ef64f1d8c08a00
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-09 19:29:18 -0800

    add test skeleton for stage 1 of algorithm

---
+++ tests/test_computer_dais.py
@@ -0,0 +1,16 @@
+import unittest
+from OnStage.computer_dais import *
+
+class ComputerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_the_computer_prefers_the_center_to_all_else(self):
+        pass
+
+    def test_the_computer_prefers_corners_to_edges(self):
+        pass
+
+    def test_the_computer_will_choose_edges_as_a_last_resort(self):
+        pass
