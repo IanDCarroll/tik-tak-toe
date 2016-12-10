c49bf1a88aed64bc6cdf5122748208bf00f39f9c
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-09 22:40:04 -0800

    refactor make_default_choice, close #23

--- OnStage/computer_dais.py
+++ OnStage/computer_dais.py
@@ -9,15 +9,8 @@ class Computer(Player):
 
     def make_default_choice(self, board):
         options = self.get_legal_moves(board)
-        if 4 in options:
-            return 4
-        if 0 in options:
-            return 0
-        if 2 in options:
-            return 2
-        if 6 in options:
-            return 6
-        if 8 in options:
-            return 8
-        else:
-            return options[0]
+        priorities = [4,0,2,6,8]
+        for priority in priorities:
+            if priority in options:
+               return priority
+        return options[0]
