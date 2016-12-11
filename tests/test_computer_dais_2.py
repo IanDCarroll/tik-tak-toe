ed48f789e15f9e28675a03c0996d37b12c4fc463
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-11 12:57:32 -0800

    deal with fork conditions later

--- tests/test_computer_dais_2.py
+++ tests/test_computer_dais_2.py
@@ -8,13 +8,9 @@ class HighPriorityComputerStrategyTestCase(unittest.TestCase):
 
         self.win_chance = [10,10,0, 0,0,0, 0,1,1]
         self.loose_chance = [10,0,1, 0,1,0, 10,0,0]
-        self.fork_chance = []
-        self.forked_chance = []
 
         self.win = [10,10,0, 0,0,0, 1,1,1]
         self.dont_loose = [10,0,1, 1,1,0, 10,0,0]
-        self.fork = []
-        self.dont_get_forked = []
 
     def test_computer_chooses_to_win(self):
         test_yields = self.hal.move(self.win_chance)
@@ -23,9 +19,3 @@ class HighPriorityComputerStrategyTestCase(unittest.TestCase):
     def test_computer_doesnt_let_you_win(self):
         test_yields = self.hal.move(self.loose_chance)
         self.assertEqual(test_yields, self.dont_loose)
-
-    def test_computer_chooses_to_fork(self):
-        pass
-
-    def test_computer_doesnt_let_you_fork(self):
-        pass
