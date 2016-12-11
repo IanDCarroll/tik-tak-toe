3e23d45bf11eadf800aa665e3a56ff04273050f9
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-11 12:45:55 -0800

    add test variables for win conditions

--- tests/test_computer_dais_2.py
+++ tests/test_computer_dais_2.py
@@ -4,7 +4,17 @@ from Onstage.computer_dais import *
 class HighPriorityComputerStrategyTestCase(unittest.TastCase):
 
     def setUp:
-        pass
+        self.hal = Computer(1)
+
+        self.win_chance = [10,10,0, 0,0,0, 0,1,1]
+        self.loose_chance = [10,0,1, 0,1,0, 10,0,0]
+        self.fork_chance = []
+        self.forked_chance = []
+
+        self.win = [10,10,0, 0,0,0, 1,1,1]
+        self.dont_loose = [10,0,1, 1,1,0, 10,0,0]
+        weld.fork = []
+        self.dont_get_forked = []
 
     def test_computer_chooses_to_win(self):
         pass
