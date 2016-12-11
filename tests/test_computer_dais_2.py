1c1db4427771fb61361ac9a2addaa5242f2efcd2
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-11 12:14:47 -0800

    add test skeleton for higher priority algorithm

---
+++ tests/test_computer_dais_2.py
@@ -0,0 +1,19 @@
+import unittest
+from Onstage.computer_dais import *
+
+class HighPriorityComputerStrategyTestCase(unittest.TastCase):
+
+    def setUp:
+        pass
+
+    def test_computer_chooses_to_win(self):
+        pass
+
+    def test_computer_doesnt_let_you_win(self):
+        pass
+
+    def test_computer_chooses_to_fork(self):
+        pass
+
+    def test_computer_doesnt_let_you_fork(self):
+        pass
