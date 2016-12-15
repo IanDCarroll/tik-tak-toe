6515de8afe53ab873d27996240d258108042e9d1
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-14 19:17:23 -0800

    add skeleton for teactical cortext tests

---
+++ tests/test_tactical_cortex_caddy.py
@@ -0,0 +1,13 @@
+import unittest
+from OnStage.tactical_cortex_caddy import *
+
+class TacticalCortexTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_take_win_chance(self):
+        pass
+
+    def test_avoid_losing(self):
+        pass
