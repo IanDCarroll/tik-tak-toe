83081a52b679d2bc40f7cdfc8cfa99a90ede7a75
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-14 19:41:52 -0800

    write variables for option conditions

--- tests/test_tactical_cortex_caddy.py
+++ tests/test_tactical_cortex_caddy.py
@@ -14,6 +14,15 @@ class TacticalCortexTestCase(unittest.TestCase):
         self.ana_5 = [0,0,0,0,0,2,0,0]
         self.ana_6 = [0,0,0,0,0,0,2,0]
         self.ana_7 = [0,0,0,0,0,0,0,2]
+        self.opt_f = [0,1,2, 3,4,5, 6,7,8]
+        self.row_0 = [0,1,   3,4,5, 6,7,8]
+        self.row_1 = [0,1,2,   4,5, 6,7,8]
+        self.row_2 = [0,1,2, 3,4,5, 6,  8]
+        self.col_3 = [0,1,2, 3,4,5,  ,7, ]
+        self.col_4 = [0,  2, 3,4,5, 6,7,8]
+        self.col_5 = [0,1,2, 3,4,   6,7,8]
+        self.diag6 = [  1,2, 3,4,5, 6,7,8]
+        self.diag7 = [0,1,2, 3,4,5,   7,8]
 
     def test_take_win_chance(self):
         pass
