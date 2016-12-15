014b68a3c124ec216aa183b74c2aa122563b8bfe
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-14 19:25:16 -0800

    write variables for analysis conditions

--- tests/test_tactical_cortex_caddy.py
+++ tests/test_tactical_cortex_caddy.py
@@ -4,7 +4,16 @@ from OnStage.tactical_cortex_caddy import *
 class TacticalCortexTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.cortex = TacticalCortex()
+        self.ana_f = [0,0,0,0,0,0,0,0]
+        self.ana_0 = [2,0,0,0,0,0,0,0]
+        self.ana_1 = [0,2,0,0,0,0,0,0]
+        self.ana_2 = [0,0,2,0,0,0,0,0]
+        self.ana_3 = [0,0,0,2,0,0,0,0]
+        self.ana_4 = [0,0,0,0,2,0,0,0]
+        self.ana_5 = [0,0,0,0,0,2,0,0]
+        self.ana_6 = [0,0,0,0,0,0,2,0]
+        self.ana_7 = [0,0,0,0,0,0,0,2]
 
     def test_take_win_chance(self):
         pass
