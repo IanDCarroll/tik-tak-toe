6f485d61893562b8d7f103a8393b9de496029776
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-14 19:12:17 -0800

    write test for take_catty_corner

--- tests/test_strategic_cortex_caddy.py
+++ tests/test_strategic_cortex_caddy.py
@@ -14,17 +14,22 @@ class StrategicCortextTestCase(unittest.TestCase):
         self.opt4 = [        4,5,  7  ]
         self.opt5 = [          5,  7  ]
         self.opt7 = [              7  ]
-        self.board_0 = [0,0,0, 0,1,0, 0,0,0]
-        self.x = 1
-        self.o = 10
-        self.optnw = [4,8]
-        self.optse = [0,4]
-        self.optne = [2,4]
-        self.optsw = [4,6]
-        self.ananw = [0,0,0,0,0,0,11,0]
-        self.anase = [0,0,0,0,0,0,11,0]
-        self.anane = [0,0,0,0,0,0,0,11]
-        self.anasw = [0,0,0,0,0,0,0,11]
+        self.bd_x = [0,0,0, 0,1,0, 0,0,0]
+        self.bd_o = [1,0,0, 0,10,0, 0,0,0]
+        self.bdnw = [10,0,0, 0,1,0, 0,0,0]
+        self.bdse = [0,0,0, 0,1,0, 0,0,10]
+        self.bdne = [0,0,10, 0,1,0, 0,0,0]
+        self.bdsw = [0,0,0, 0,1,0, 10,0,0]
+        self.optnw = [  1,2,3,  5,6,7,8]
+        self.optse = [0,1,2,3,  5,6,7  ]
+        self.optne = [0,1,  3,  5,6,7,8]
+        self.optsw = [0,1,2,3,  5,  7,8]
+        self.ana_x = [0,1,0,0,1,0,1,1]
+        self.ana_o = [0,10,0,0,10,0,11,10]
+        self.ananw = [10,1,0,10,1,0,11,1]
+        self.anase = [0,1,10,0,1,10,11,1]
+        self.anane = [10,1,0,0,1,10,1,11]
+        self.anasw = [0,1,10,10,1,0,1,11]
 
     def test_make_default_choice_chooses_the_right_order(self):
         dic0 = {'options': self.opt0}
@@ -56,7 +61,30 @@ class StrategicCortextTestCase(unittest.TestCase):
         self.assertEqual(test_8, 8)
 
     def test_take_catty_corner_does_appropriately(self):
-        pass
+        dic_x = { 'board': self.bd_x, 'analysis': self.ana_x,
+                  'marker_code': 1, 'options': self.optnw }
+        dic_o = { 'board': self.bd_o, 'analysis': self.ana_o,
+                  'marker_code': 1, 'options': self.optnw }
+        dicnw = { 'board': self.bdnw, 'analysis': self.ananw,
+                  'marker_code': 1, 'options': self.optnw }
+        dicse = { 'board': self.bdse, 'analysis': self.anase, 
+                  'marker_code': 1, 'options': self.optse }
+        dicne = { 'board': self.bdne, 'analysis': self.anane,
+                  'marker_code': 1, 'options': self.optne }
+        dicsw = { 'board': self.bdsw, 'analysis': self.anasw,
+                  'marker_code': 1, 'options': self.optsw }
+        test_x = self.cortex.take_catty_corner(dic_x)
+        test_o = self.cortex.take_catty_corner(dic_o)
+        testnw = self.cortex.take_catty_corner(dicnw)
+        testse = self.cortex.take_catty_corner(dicse)
+        testne = self.cortex.take_catty_corner(dicne)
+        testsw = self.cortex.take_catty_corner(dicsw)
+        self.assertEqual(test_x, False)
+        self.assertEqual(test_o, False)
+        self.assertEqual(testnw, 8)
+        self.assertEqual(testse, 0)
+        self.assertEqual(testne, 6)
+        self.assertEqual(testsw, 2)
 
     def test_take_the_center_takes_the_center(self):
         dic0 = {'options': self.opt0}
