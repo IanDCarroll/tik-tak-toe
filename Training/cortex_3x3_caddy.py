fabc7c8f1968cd5f954332097681594c5ecfc8ee
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-19 12:18:03 -0800

    add new files not staged in last commit

---
+++ Training/cortex_3x3_caddy.py
@@ -0,0 +1,24 @@
+from strategic_3x3_lobe_slot import *
+from tactical_3x3_lobe_slot import *
+
+class Cortex_3x3(object):
+
+    strat = StrategicLobe()
+    tac = TacticalLobe()
+
+    def direct_move(self, intel):
+        priority_list = self.ask_lobes(intel)
+        for priority in priority_list:
+            if priority is not False:
+	        return priority
+
+    def ask_lobes(self, intel):       
+        p1 = self.tac.take_win_chance(intel)
+        p2 = self.tac.avoid_losing(intel)
+        p3 = self.tac.take_fork_chance(intel)
+        p4 = self.tac.avoid_fork(intel)
+        p5 = self.strat.take_the_center(intel)
+        p6 = self.strat.take_catty_corner(intel)
+        p7 = self.strat.make_default_choice(intel)
+        priority_list = [p1, p2, p3, p4, p5, p6, p7]
+        return priority_list
---
+++ tests/test_cortex_3x3_caddy.py
@@ -0,0 +1,22 @@
+import unittest
+from Training.cortex_3x3_caddy import *
+
+class Cortex3x3TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.cortex = Cortex_3x3()
+        self.intel = { 'board': [10,10,0, 1,1,0, 10,0,1],
+                       'options': [2,5,7], 
+                       'analysis': [20,2,11, 21,11,1, 12,11],
+                       'marker_code': 1,
+                       'enemy_code': 10 }
+        self.priorities = [5,2,False,False,False,2,2]
+
+
+    def test_ask_lobes_returns_a_priority_list(self):
+        test_yields = self.cortex.ask_lobes(self.intel)
+        self.assertEqual(test_yields, self.priorities)
+
+    def test_direct_move_returns_a_direction(self):
+        test_yields = self.cortex.direct_move(self.intel)
+        self.assertEqual(test_yields, 5)
