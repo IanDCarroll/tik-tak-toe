964870355bc08f8cc7a846b394da571847482c23
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-18 16:59:28 -0800

    add test conditions for fork avoidance

--- tests/test_tactical_cortex_caddy.py
+++ tests/test_tactical_cortex_caddy.py
@@ -58,6 +58,8 @@ class TacticalCortexTestCase(unittest.TestCase):
         self.fork_with_6b = { 'board': [10,0,0, 0,1,10, 0,0,1] }
         self.fork_with_8a = { 'board': [0,10,1, 0,1,0, 10,0,0] }
         self.fork_with_8b = { 'board': [0,0,10, 10,1,0, 1,0,0] }
+        self.avoid_fork_1 = { 'board': [1,0,0, 0,10,0, 0,0,1] }
+        self.avoid_fork_2 = { 'board': [0,0,1, 0,10,0, 1,0,0] }
 
     def test_take_win_chance(self):
         dic_f = { 'analysis': self.ana_f,'options': self.opt_f,
