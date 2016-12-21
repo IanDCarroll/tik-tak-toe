3d9d28fe38e0722a3dae3c9f3a0e6481dd0c7657
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-20 18:32:58 -0800

    refactor parser with better names

--- Training/parser_3x3_abilities.py
+++ Training/parser_3x3_abilities.py
@@ -1,15 +1,15 @@
 class Parser(object):
 
-   def get_empty_square(self, options, code):
-       possibilities = ((0,1,2),(3,4,5),(6,7,8),
-                        (0,3,6),(1,4,7),(2,5,8),
-                        (0,4,8),(2,4,6))
+   def get_empty_square(self, options, look_up):
+       lines = ((0,1,2),(3,4,5),(6,7,8),
+                (0,3,6),(1,4,7),(2,5,8),
+                (0,4,8),(2,4,6))
 
-       for possibility in possibilities:
-           if possibilities.index(possibility) == code:
-               return self.find_empty_spot(options, possibility)
+       for line in lines:
+           if lines.index(line) == look_up:
+               return self.find_empty_spot(options, line)
 
-   def find_empty_spot(self, options, index_list):
-       for spot in index_list:
+   def find_empty_spot(self, options, spots):
+       for spot in spots:
            if spot in options:
               return spot
