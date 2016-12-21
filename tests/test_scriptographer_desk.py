41a69e7b8806cbf686705480c90947a6a92f33d9
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-20 21:17:46 -0800

    test revisions of sayings that the game uses

--- tests/test_scriptographer_desk.py
+++ tests/test_scriptographer_desk.py
@@ -5,30 +5,23 @@ class ScriptographerTestCase(unittest.TestCase):
 
     def setUp(self):
         self.say = Scriptographer()
-        self.start = """
-Welcome to XOX, 
+        self.start = """Welcome to XOX, 
 a Noughts and Crosses Game you can never win 
 no matter how hard you try."""
-        self.select = """
-Type 1 to go first and not win, or 
+        self.select = """Type 1 to go first and not win, or 
 Type 2 to go second and not win."""
-        self.tie = """
-Your game-play was practically non-incompetent. 
+        self.tie = """Your moves were practically non-incompetent. 
 The game is a draw."""
-        self.computer = """
-The Computer has exploited your pathetic human weaknesses 
-and won the game, as is correct."""
-        self.human = """
-What?! You Won? Impossible!
+        self.computer = """The computer has deftly exploited 
+your pathetic human weaknesses and won the game."""
+        self.human = """What?! You Won? Impossible!
 Open an issue at https://github.com/IanDCarroll/xox/issues/new 
-so it can be corrected immediately!"""
-        self.question = """
-Which square do you choose before your inevitable failure to win?"""
-        self.pre_choice = "\nThe computer chooses square "
+so it can be corrected immediately."""
+        self.question = "Which square do you choose before your inevitable failure to win?"
+        self.pre_choice = "The computer chooses square "
         self.post_choice = ", a perfect move."
-        self.bad_move = "Sorry, that's not a legal move. Try again."
-        self.strike_3 = """
-Look, if you're not going to take this seriously, I'm out."""
+        self.bad_move = "That's not a legal move, human. Try again."
+        self.strike_3 = "Look, if you're not going to take this seriously, I'm out."
         self.prompt = "> "
 
         self.nought = "\033[34m O \033[0m"
