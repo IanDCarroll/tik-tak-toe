937cccc02648f8725f7eb9e109f092308c04e263
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-30 12:38:00 -0800

    remove unnecessary assignment statement

--- Training/cortex_3x3_caddy.py
+++ Training/cortex_3x3_caddy.py
@@ -20,5 +20,4 @@ class Cortex_3x3(object):
         p5 = self.strat.take_the_center(intel)
         p6 = self.strat.take_catty_corner(intel)
         p7 = self.strat.make_default_choice(intel)
-        priority_list = [p1, p2, p3, p4, p5, p6, p7]
-        return priority_list
+        return [p1, p2, p3, p4, p5, p6, p7]
