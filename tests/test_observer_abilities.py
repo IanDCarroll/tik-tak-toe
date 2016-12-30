1a9fef6c81aff0cd069774f16b59cedc40ae7d69
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-30 12:03:06 -0800

    change names of scan_diag methods in test

--- tests/test_observer_abilities.py
+++ tests/test_observer_abilities.py
@@ -18,8 +18,8 @@ class ObserverTestCase(unittest.TestCase):
         self.expected_rows = [11,11,12]
         self.expected_cols = [3,20,11]
         self.expected_diags = [12,21]
-        self.expected_NW_SE_value = 12
-        self.expected_NE_SW_value = 21
+        self.expected_1st_value = 12
+        self.expected_2nd_value = 21
 
     def test_get_board_size_3(self):
         test = self.observer.get_board_size(self.mock_board)
@@ -49,13 +49,13 @@ class ObserverTestCase(unittest.TestCase):
         test_yields = self.observer.scan_diags(self.mock_board)
         self.assertEqual(test_yields, self.expected_diags)
 
-    def test_scan_NW_SE_diag_returns_analyzed_value(self):
-        test_yields = self.observer.scan_NW_SE(self.mock_board)
-        self.assertEqual(test_yields, self.expected_NW_SE_value)
+    def test_scan_first_diag_returns_analyzed_value(self):
+        test_yields = self.observer.scan_1st_diag(self.mock_board)
+        self.assertEqual(test_yields, self.expected_1st_value)
 
-    def test_scan_NE_SW_diag_returns_analyzed_value(self):
-        test_yields = self.observer.scan_NE_SW(self.mock_board)
-        self.assertEqual(test_yields, self.expected_NE_SW_value)
+    def test_scan_second_diag_returns_analyzed_value(self):
+        test_yields = self.observer.scan_2nd_diag(self.mock_board)
+        self.assertEqual(test_yields, self.expected_2nd_value)
 
 if __name__ == '__main__':
     unittest.main()
