05fe50c1427ef9e95da2d99a64c4b5df221e2978
IanDCarroll <ian.d.carroll@gmail.com>
2017-01-30 13:16:00 -0800

    test refresh method exists in TerminalInterface

--- tests/test_cli_display.py
+++ tests/test_cli_display.py
@@ -5,18 +5,21 @@ class Helper(object):
      def get_methods(self, obj):
          return [i for i in dir(obj) if callable(getattr(obj, i))]
 
-class AnnouncerTestCase(unittest.TestCase):
+class TerminalInterfaceTestCase(unittest.TestCase):
 
     def setUp(self):
         self.ui = TerminalInterface("fake_board_object")
         self.helper = Helper()
         self.methodList = self.helper.get_methods(self.ui)
 
-    def test_announcer_is_a_class(self):
+    def test_terminalinterface_is_a_class(self):
         self.assertIsInstance(self.ui, TerminalInterface)
 
-    def test_announcer_has_a_show_method(self):
+    def test_terminalinterface_has_a_show_method(self):
         self.assertTrue('show' in self.methodList)
 
-    def test_announcer_has_an_ask_human_method(self):
+    def test_terminalinterface_has_an_ask_human_method(self):
         self.assertTrue('ask_human' in self.methodList)
+
+    def testterminalinterface_has_a_refresh_method(self):
+        self.assertTrue('refresh' in self.methodList)
