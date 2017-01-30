377b58df5cf132e08e79b13896ead406613e00d3
IanDCarroll <ian.d.carroll@gmail.com>
2017-01-30 11:14:50 -0800

    reformat cli_display

--- Scenery/cli_display.py
+++ Scenery/cli_display.py
@@ -2,14 +2,14 @@ from carpenter_shop import *
 
 class TerminalInterface(Carpenter):
 
-    def show(self, text):
-        print text
-
-    def ask_human(self):
-        return raw_input(self.prompt)
-
     def show_computer_choice(self, choice):
         pre = self.pre_choice
         post = self.post_choice
         statement = pre + str(choice + 1) + post
         self.show(statement)
+
+    def show(self, text):
+        print text
+
+    def ask_human(self):
+        return raw_input(self.prompt)
