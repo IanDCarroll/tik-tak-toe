f1b7c9f8b8221ffb3d4a5bda6ae0e5480d41de1d
IanDCarroll <ian.d.carroll@gmail.com>
2017-02-10 15:19:01 -0800

    assert win displays the player whose turn it is

--- tests/test_cli_display.py
+++ tests/test_cli_display.py
@@ -16,6 +16,7 @@ class TerminalInterfaceTestCase(unittest.TestCase):
         self.start_message = '\nWelcome to XOX, \na Noughts and Crosses Game you can never win \nno matter how hard you try.\n\nType 1 to go first and not win, or \nType 2 to go second and not win.'
         self.error_message = 'That\'s not a legal move, human. Try again.'
         self.tie_message = '\n\x1b[90m 1 \x1b[0m|\x1b[90m 2 \x1b[0m|\x1b[90m 3 \x1b[0m\n---+---+---\n\x1b[90m 4 \x1b[0m|\x1b[90m 5 \x1b[0m|\x1b[90m 6 \x1b[0m\n---+---+---\n\x1b[90m 7 \x1b[0m|\x1b[90m 8 \x1b[0m|\x1b[90m 9 \x1b[0m\n\nYour moves were practically non-incompetent. \nThe game is a draw.\n'
+        self.win_message = '\n\x1b[90m 1 \x1b[0m|\x1b[90m 2 \x1b[0m|\x1b[90m 3 \x1b[0m\n---+---+---\n\x1b[90m 4 \x1b[0m|\x1b[90m 5 \x1b[0m|\x1b[90m 6 \x1b[0m\n---+---+---\n\x1b[90m 7 \x1b[0m|\x1b[90m 8 \x1b[0m|\x1b[90m 9 \x1b[0m\n\nWhat?! You Won? Impossible!\nOpen an issue at https://github.com/IanDCarroll/xox/issues/new \nso it can be corrected immediately.\n'
 
     def test_terminalinterface_is_a_class(self):
         self.assertIsInstance(self.ui, TerminalInterface)
@@ -40,3 +41,8 @@ class TerminalInterfaceTestCase(unittest.TestCase):
         test_tie = self.ui.display()
         self.assertEqual(test_tie, self.tie_message)
         self.table_top.tie = False
+
+        self.table_top.win = True
+        test_win = self.ui.display()
+        self.assertEqual(test_win, self.win_message)
+        self.table_top.win = False
