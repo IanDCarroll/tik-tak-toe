8f62ae55a939608f61048921b86939e18850910a
IanDCarroll <ian.d.carroll@gmail.com>
2017-02-10 15:13:54 -0800

    assert display displays an expected tie message

--- tests/test_cli_display.py
+++ tests/test_cli_display.py
@@ -15,6 +15,7 @@ class TerminalInterfaceTestCase(unittest.TestCase):
         self.methodList = self.helper.get_methods(self.ui)
         self.start_message = '\nWelcome to XOX, \na Noughts and Crosses Game you can never win \nno matter how hard you try.\n\nType 1 to go first and not win, or \nType 2 to go second and not win.'
         self.error_message = 'That\'s not a legal move, human. Try again.'
+        self.tie_message = '\n\x1b[90m 1 \x1b[0m|\x1b[90m 2 \x1b[0m|\x1b[90m 3 \x1b[0m\n---+---+---\n\x1b[90m 4 \x1b[0m|\x1b[90m 5 \x1b[0m|\x1b[90m 6 \x1b[0m\n---+---+---\n\x1b[90m 7 \x1b[0m|\x1b[90m 8 \x1b[0m|\x1b[90m 9 \x1b[0m\n\nYour moves were practically non-incompetent. \nThe game is a draw.\n'
 
     def test_terminalinterface_is_a_class(self):
         self.assertIsInstance(self.ui, TerminalInterface)
@@ -29,7 +30,13 @@ class TerminalInterfaceTestCase(unittest.TestCase):
         test_start = self.ui.display()
         self.assertEqual(test_start, self.start_message)
         self.assertEqual(self.table_top.start_of_game, False)
+
         self.table_top.error = True
         test_error = self.ui.display()
         self.assertEqual(test_error, self.error_message)
         self.assertEqual(self.table_top.error, False)
+
+        self.table_top.tie = True
+        test_tie = self.ui.display()
+        self.assertEqual(test_tie, self.tie_message)
+        self.table_top.tie = False
